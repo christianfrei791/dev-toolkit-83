26e56ad0ff29ec67853490c61daa1c73ad1a23f1
Christian Frei <christianfrei791@gmail.com>
2026-08-16 16:15:19 +0200

    Add configuration management helper functions

--- config.py
+++ config.py
@@ -1,28 +1,28 @@
 import json
-import os
 
-DEFAULT_CONFIG = {
-    'click_interval': 0.1,
-    'max_clicks': 1000,
-    'click_type': 'left',
-    'run_in_background': False
-}
-
-class ConfigLoader:
-    def __init__(self, config_file='config.json'):
+class Config:
+    def __init__(self, config_file):
         self.config_file = config_file
-        self.config = self.load_config()
+        self.settings = self.load_config()
 
     def load_config(self):
-        if os.path.exists(self.config_file):
+        try:
             with open(self.config_file, 'r') as file:
-                user_config = json.load(file)
-            return {**DEFAULT_CONFIG, **user_config}
-        return DEFAULT_CONFIG
+                return json.load(file)
+        except FileNotFoundError:
+            print(f'Config file {self.config_file} not found.')
+            return {}
+        except json.JSONDecodeError:
+            print('Error decoding JSON from the config file.')
+            return {}
 
-    def get(self, key):
-        return self.config.get(key, DEFAULT_CONFIG.get(key))
+    def get_setting(self, key, default=None):
+        return self.settings.get(key, default)
 
-if __name__ == '__main__':
-    loader = ConfigLoader()
-    print(loader.config)
+    def set_setting(self, key, value):
+        self.settings[key] = value
+        self.save_config()
+
+    def save_config(self):
+        with open(self.config_file, 'w') as file:
+            json.dump(self.settings, file, indent=4)
